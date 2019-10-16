0eabd92c2d0d2110de8912ad2141d5fe8311279d
Wen Sun <syl1887415157@126.com>
2019-10-16 23:59:12 +0800

    feat: add os_test

---
+++ boot.asm
@@ -0,0 +1,18 @@
+org 0x7C00
+mov ax,cs
+mov es,ax
+call    DispStr
+jmp $
+
+DispStr:
+    mov bp,BootMessage
+    mov cx,10
+    mov ax,0x1301
+    mov bx,0x000C
+    mov dx,0x0200
+    int 10H
+    ret
+BootMessage:        db "Hello, OS!"
+
+times   510-($-$$)  db 0
+dw  0xAA55
